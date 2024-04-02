b88557b834befc0a97e2f4e6aad2b2cd634e9d75
Leo Almeida <144178640+AlmeidaLeo7b@users.noreply.github.com>
2024-04-02 15:28:32 -0300

    Add files via upload

---
+++ Fibonacci/Fibonacci.java
@@ -0,0 +1,34 @@
+public class Fibonacci {
+    private int i;
+
+    public Fibonacci(int i) {
+        this.i = i;
+    }
+    public int calcularFibonacci(int n) {
+        if (n <= 1) {
+            return n;
+        } else {
+            return calcularFibonacci(n - 1) + calcularFibonacci(n - 2);
+        }
+    }
+    public int pertenceFibonacci(int num) {
+        int a = 0;
+        int b = 1;
+
+
+        while (a <= num) {
+            if (a == num) {
+                System.out.println("\n O numero "+ num + " pertence na sequencia de Fibonacci");
+                return 0;
+            }
+            int temp = a;
+            a = b;
+            b = temp + b;
+
+        }
+        System.out.println("\n O numero "+ num + " nao pertence na sequencia de Fibonacci");
+        return 0;
+    }
+}
+
+
---
+++ Fibonacci/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public static void main(String[] args) {
+    int n;
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.println("informado um numero: ");
+    n =  scanner.nextInt();
+
+   Fibonacci fb = new Fibonacci(n);
+
+    System.out.println("Sequência de Fibonacci com " + n + " termos: \n");
+
+    for (int i = 0; i < n; i++) {
+        System.out.print(fb.calcularFibonacci(i) + " ");
+    }
+    fb.pertenceFibonacci(n);
+}
